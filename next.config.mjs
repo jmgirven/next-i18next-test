32ff323158346a9b966b35c20f22e0b3eec20186
Jon Girven <j.m.girven@gmail.com>
2024-03-21 11:10:25 +0000

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
+import i18nConfig from "./next-i18next.config.js";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  i18n: i18nConfig.i18n,
   reactStrictMode: true,
 };
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
-import Image from "next/image";
+import { Trans } from "next-i18next";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import { Inter } from "next/font/google";
+import Image from "next/image";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -10,7 +12,8 @@ export default function Home() {
     >
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
+          <Trans i18nKey={"common:getStarted"} />
+          &nbsp;
           <code className="font-mono font-bold">src/pages/index.tsx</code>
         </p>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
@@ -115,4 +118,16 @@ export default function Home() {
       </div>
     </main>
   );
+}
+
+type Options = {
+  locale: string | undefined;
+};
+
+export async function getStaticProps({ locale }: Options) {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale || "en", ["common", "footer"])),
+    },
+  };
 }
